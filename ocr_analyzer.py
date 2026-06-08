cf52eb993ef224871d27703ed2b04afd04999f1b
Venu Kesavaram K <venukesavaraamk@gmail.com>
2026-06-08 12:17:25 +0530

    Fix Tesseract path for Streamlit Cloud

--- ocr_analyzer.py
+++ ocr_analyzer.py
@@ -1,10 +1,11 @@
 import pytesseract
 import difflib
 import cv2
-
-pytesseract.pytesseract.tesseract_cmd = (
-    r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-)
+import os
+if os.name == "nt":
+    pytesseract.pytesseract.tesseract_cmd = (
+        r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+    )
 
 
 def extract_text(image_path):
